29a6a7b25a7fc92746003ea92bc1a048711dd1bd
Sai Bhargav <saibhargav@Sais-MacBook-Air-2.local>
2022-05-05 23:42:10 -0400

    Intial commit

---
+++ src/components/Game.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+       <h2>{{game.title}}</h2>
+       <h4>{{game.year}} - {{game.company}}</h4>
+       <h4>{{game.genre}} - {{game.rating}} star</h4>
+    </div>
+   
+   
+</template>
+
+<script>
+
+export default{
+    name: 'Game',
+    props:{
+        game: Array
+    }
+   
+}
+</script>
+
+<style scoped>
+
+h4{
+    margin: 2px;
+}
+
+
+div{
+    background-color: #ffd60a;
+    color: #003566;
+    padding: 1em;
+}
+
+
+
+</style>
---
+++ src/components/Playstationbox.vue
@@ -0,0 +1,46 @@
+<template>
+    
+    <div class="gamebox">
+        
+        <div v-for="game in games" :key="game.id" >
+            <Game :game="game"/>
+     
+                <!--<div><h3> {{game[0].title}} </h3></div>
+                <div><h3> {{game[1].title}} </h3></div>
+                <div><h3> {{game[2].title}} </h3></div>
+                <div><h3> {{game[3].title}} </h3></div>
+                <div><h3> {{game[4].title}} </h3></div>-->
+                
+        
+        </div>
+       
+    </div>
+
+</template>
+
+<script>
+
+import Game from './Game.vue'
+export default{
+
+    name: 'Playstationbox',
+    components: {
+        Game
+    },
+    props: {
+        games: Array
+    }
+}
+
+</script>
+
+<style scoped>
+
+.gamebox{
+
+    background-color:#14213d;
+    padding: 1em;
+    color: yellow;
+}
+
+</style>
